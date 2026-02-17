f35bac6081f71a7ae465d98bac876726e0a2f6f7
phantriviethoang <phantriviethoang@gmail.com>
2026-02-17 23:15:37 +0700

    Creating Reusable Layouts with React Children

--- resources/js/pages/about.tsx
+++ resources/js/pages/about.tsx
@@ -1,11 +1,12 @@
 import { Link } from "@inertiajs/react";
+import AppLayout from "../layouts/app-layout";
 
 export default function About() {
     return (
-        <div>
+        <AppLayout>
             <h1>Home</h1>
             <div>Welcome to about page</div>
             <Link href="/">Home</Link>
-        </div>
+        </AppLayout>
     );
 }
--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,11 +1,12 @@
 import { Link } from "@inertiajs/react";
+import AppLayout from "../layouts/app-layout";
 
 export default function Home() {
     return (
-        <div>
+        <AppLayout>
             <h1>Home</h1>
             <div>Welcome to homepage</div>
             <Link href="/about">About</Link>
-        </div>
+        </AppLayout>
     );
 }
