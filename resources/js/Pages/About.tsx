efe4652774210bd824683fea6cb880a55d398c21
phantriviethoang <phantriviethoang@gmail.com>
2026-02-17 23:03:26 +0700

    Client-Side Navigation with Link Component

--- resources/js/Pages/About.tsx
+++ resources/js/Pages/About.tsx
@@ -1,8 +1,11 @@
+import { Link } from "@inertiajs/react";
+
 export default function About() {
     return (
         <div>
             <h1>Home</h1>
             <div>Welcome to about page</div>
+            <Link href="/">Home</Link>
         </div>
     );
 }
