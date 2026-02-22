46b760be7338aea60eed74d54f14faec328564f4
phantriviethoang <phantriviethoang@gmail.com>
2026-02-22 14:29:39 +0700

    Creating Posts with Inertia Forms

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -21,5 +23,25 @@ class PostController extends Controller
         return Inertia::render('posts/show', [
             'post' => Post::findOrFail($id),
         ]);
+    }
+
+    public function create(): Response
+    {
+        return Inertia::render('posts/create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        // Validate
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required|string|max:255',
+        ]);
+
+        // Create the post in the db
+        Post::create($validated);
+
+        // redirect to /posts
+        return redirect('/posts');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ Route::get('/about', function () {
     return Inertia::render('about');
 });
 
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
