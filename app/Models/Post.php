193a75f0ca2e7b63e44a0c24374f14c69c3a3a6f
phantriviethoang <phantriviethoang@gmail.com>
2026-02-22 15:06:09 +0700

    Defining One-to-Many Relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    protected $fillable = ['title' , 'body'];
+    protected $fillable = ['title', 'body'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
